28cd757224b5021d69d031a69e4900ee8e3f21b2
wz <wenzhe1423@gmail.com>
2026-04-17 20:57:06 +0800

    feat: email can send attachments

--- Backend/newsletter.py
+++ Backend/newsletter.py
@@ -1,7 +1,5 @@
 # Module to Send a Generated Report for the User
 import os
-import smtplib
-from email.mime.text import MIMEText
 
 import dotenv
 import resend
@@ -13,13 +11,33 @@ resend.api_key = os.getenv("RESEND_API_KEY")
 email_address = os.getenv("EMAIL")
 
 
+def decode_attachment(filepath):
+    f: bytes = open(os.path.join(os.path.dirname(__file__), filepath), "rb").read()
+    return f
+
+
 # Send Email to User (TODO: Add attachments into email)
 def send_email(report, spreadsheet):
+    report = decode_attachment("./Sample_WordFile.docx")
+    spreadsheet = decode_attachment("./Sample_Spreadsheet.xlsx")
+
+    # Convert Bytes into Attachment item
+    report_attachment: resend.Attachment = {
+        "content": list(report),
+        "filename": "report.docx",
+    }
+
+    spreadsheet_attachment: resend.Attachment = {
+        "content": list(spreadsheet),
+        "filename": "spreadsheet.xlsx",
+    }
+
     params: resend.Emails.SendParams = {
         "from": "onboarding@resend.dev",
         "to": [email_address],
         "subject": "Testing Email",
         "html": "<strong>it works!</strong><br/>Hello",
+        "attachments": [report_attachment, spreadsheet_attachment],
     }
 
     email = resend.Emails.send(params)
