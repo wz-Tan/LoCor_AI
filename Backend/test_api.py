5a6b3c612419a382cfd74db01ae2bdf80985fc12
Colin <colinleonghongseng@gmail.com>
2026-04-17 16:23:32 +0800

    separated out for-human for-api

--- Backend/test_api.py
+++ Backend/test_api.py
@@ -2,22 +2,23 @@
 This file is to add APIs from RapidAPI before integration.
 
 How to Test?
-1. Edit main() function and choose an API from the API_MAP
-2. If you see {'message': 'You are not subscribed to this API.'}, go to step 3
-3. Subscribe to free tier of the selected API based on the url (Commented out)
+1. Comment out 1 of the 2 lines in main()
+2. Run main() function with `python Backend/test_api.py`
+3. If you see {'message': 'You are not subscribed to this API.'}, go to step 3
+4. Subscribe to free tier of the selected API at 'api-info' at 'for-human'
+5. Don't run all APIs together or you'll have no more quotas left...
 
 How to Add New API?
 1. Browse your desired API on RapidAPI
 2. Ensure the free plan is enough to be used (> 100 / month)
-3. Add the required fields as per the example (name, url, sample payload, x-rapidapi-host, api-key-name)
-4. Add the API key to .env file
-5. Include the url to the API (Commented out) so others could find the API
+3. Add the required fields as per the examples ('for-api' is used when calling the API, 'for-human' is just for us to see)
+4. Add the API key to .env file (All APIs on RapidAPI uses same API_KEY)
 
 !!! FOR YOUR INFORMATION
-The url commented out (https://rapidapi.com/ytjar/api/yt-api)
-and the url for the API (https://yt-api.p.rapidapi.com/trending)
-IS NOT THE SAME!!!
+The url for humans (https://rapidapi.com/ytjar/api/yt-api)
+The url for the API (https://yt-api.p.rapidapi.com/trending)
 
+ARE NOT THE SAME!!!
 We need 1 for the API, 1 for us to browse (Please include both)
 '''
 
@@ -28,23 +29,74 @@ import os
 load_dotenv()
 
 API_MAP = {
-    # https://rapidapi.com/ytjar/api/yt-api
-    'YT-API': {
-        'url': 'https://yt-api.p.rapidapi.com/trending',
-        'payload': {
-            'geo': 'US',
-            'type': 'now'
+    # 1
+    'youtube-trending': {
+        'for-human': {
+            'api-info': 'https://rapidapi.com/ytjar/api/yt-api',
+            'free-limit': '300/month'
         },
-        'x-rapidapi-host': 'yt-api.p.rapidapi.com',
-        'api-key-name': 'YT_API_KEY'
-    }
+
+        'for-api': {
+            'url': 'https://yt-api.p.rapidapi.com/trending',
+            'payload': {
+                'geo': 'US',
+                'type': 'now'
+            },
+            'x-rapidapi-host': 'yt-api.p.rapidapi.com',
+        }
+    },
+
+    # 2
+    'tiktok-trending': {
+        'for-human': {
+            'api-info': 'https://rapidapi.com/tikwm-tikwm-default/api/tiktok-scraper7',
+            'free-limit': '300/month'
+        },
+
+        'for-api': {
+            'url': 'https://tiktok-scraper7.p.rapidapi.com/feed/list',
+            'payload': {
+                'region': 'us',
+                'count': '10'
+            },
+            'x-rapidapi-host': 'tiktok-scraper7.p.rapidapi.com',
+        }
+    },
+
+    # 3
+    'tiktok-trending-2': {
+        'for-human': {
+            'api-info': 'https://rapidapi.com/ponds4552/api/tiktok-best-experience',
+            'free-limit': '100/day'     # So generous ah
+        },
+
+        'for-api': {
+            'url': 'https://tiktok-best-experience.p.rapidapi.com/trending',
+            'payload': None,
+            'x-rapidapi-host': 'tiktok-best-experience.p.rapidapi.com',
+        }
+    },
+
+    # 4
+    'news': {
+        'for-human': {
+            'api-info': 'https://rapidapi.com/bonaipowered/api/news-api14',
+            'free-limit': '100/day'     # So generous x2
+        },
+
+        'for-api': {
+            'url': 'https://news-api14.p.rapidapi.com/v2/trendings',
+            'payload': None,
+            'x-rapidapi-host': 'news-api14.p.rapidapi.com',
+        }
+    },
 }
 
 def test_api(api_name):
-    api = API_MAP[api_name]
+    api = API_MAP[api_name]['for-api']
 
     headers = {
-        'x-rapidapi-key': os.getenv(api['api-key-name']),
+        'x-rapidapi-key': os.getenv('API_KEY'),
         'x-rapidapi-host': api['x-rapidapi-host'],
         'Content-Type': 'application/json'
     }
@@ -56,9 +108,14 @@ def test_api(api_name):
     )
 
 def main():
-    # Edit name based on the API you want to test
-    response = test_api('YT-API')
-    print(response.json())
+    # Comment out either one
+    apis_to_test = API_MAP.keys()       # Test all (Not recommended)
+    apis_to_test = API_MAP['api-name']  # Test desired (Change name)
+
+    for api in apis_to_test:
+        print(f'Testing API: "{api}"')
+        response = test_api(api)
+        print(response.json(), end='\n\n')
 
 if __name__ == '__main__':
     main()
