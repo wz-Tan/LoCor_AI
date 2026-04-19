23d52b3e7366d5c1411f4c5afca6ce10f7baf3d2
wz <wenzhe1423@gmail.com>
2026-04-19 10:43:20 +0800

    chore: env example file

--- Backend/test_api.py
+++ Backend/test_api.py
@@ -1,4 +1,4 @@
-'''
+"""
 This file is to add APIs from RapidAPI before integration.
 
 How to Test?
@@ -20,102 +20,89 @@ The url for the API (https://yt-api.p.rapidapi.com/trending)
 
 ARE NOT THE SAME!!!
 We need 1 for the API, 1 for us to browse (Please include both)
-'''
+"""
+
+import os
 
 import requests
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
 API_MAP = {
     # 1
-    'youtube-trending': {
-        'for-human': {
-            'api-info': 'https://rapidapi.com/ytjar/api/yt-api',
-            'free-limit': '300/month'
+    "youtube-trending": {
+        "for-human": {
+            "api-info": "https://rapidapi.com/ytjar/api/yt-api",
+            "free-limit": "300/month",
         },
-
-        'for-api': {
-            'url': 'https://yt-api.p.rapidapi.com/trending',
-            'payload': {
-                'geo': 'US',
-                'type': 'now'
-            },
-            'x-rapidapi-host': 'yt-api.p.rapidapi.com',
-        }
+        "for-api": {
+            "url": "https://yt-api.p.rapidapi.com/trending",
+            "payload": {"geo": "US", "type": "now"},
+            "x-rapidapi-host": "yt-api.p.rapidapi.com",
+        },
     },
-
     # 2
-    'tiktok-trending': {
-        'for-human': {
-            'api-info': 'https://rapidapi.com/tikwm-tikwm-default/api/tiktok-scraper7',
-            'free-limit': '300/month'
+    "tiktok-trending": {
+        "for-human": {
+            "api-info": "https://rapidapi.com/tikwm-tikwm-default/api/tiktok-scraper7",
+            "free-limit": "300/month",
         },
-
-        'for-api': {
-            'url': 'https://tiktok-scraper7.p.rapidapi.com/feed/list',
-            'payload': {
-                'region': 'us',
-                'count': '10'
-            },
-            'x-rapidapi-host': 'tiktok-scraper7.p.rapidapi.com',
-        }
+        "for-api": {
+            "url": "https://tiktok-scraper7.p.rapidapi.com/feed/list",
+            "payload": {"region": "us", "count": "10"},
+            "x-rapidapi-host": "tiktok-scraper7.p.rapidapi.com",
+        },
     },
-
     # 3
-    'tiktok-trending-2': {
-        'for-human': {
-            'api-info': 'https://rapidapi.com/ponds4552/api/tiktok-best-experience',
-            'free-limit': '100/day'     # So generous ah
+    "tiktok-trending-2": {
+        "for-human": {
+            "api-info": "https://rapidapi.com/ponds4552/api/tiktok-best-experience",
+            "free-limit": "100/day",  # So generous ah
         },
-
-        'for-api': {
-            'url': 'https://tiktok-best-experience.p.rapidapi.com/trending',
-            'payload': None,
-            'x-rapidapi-host': 'tiktok-best-experience.p.rapidapi.com',
-        }
+        "for-api": {
+            "url": "https://tiktok-best-experience.p.rapidapi.com/trending",
+            "payload": None,
+            "x-rapidapi-host": "tiktok-best-experience.p.rapidapi.com",
+        },
     },
-
     # 4
-    'news': {
-        'for-human': {
-            'api-info': 'https://rapidapi.com/bonaipowered/api/news-api14',
-            'free-limit': '100/day'     # So generous x2
+    "news": {
+        "for-human": {
+            "api-info": "https://rapidapi.com/bonaipowered/api/news-api14",
+            "free-limit": "100/day",  # So generous x2
         },
-
-        'for-api': {
-            'url': 'https://news-api14.p.rapidapi.com/v2/trendings',
-            'payload': None,
-            'x-rapidapi-host': 'news-api14.p.rapidapi.com',
-        }
+        "for-api": {
+            "url": "https://news-api14.p.rapidapi.com/v2/trendings",
+            "payload": None,
+            "x-rapidapi-host": "news-api14.p.rapidapi.com",
+        },
     },
 }
 
+
 def test_api(api_name):
-    api = API_MAP[api_name]['for-api']
+    api = API_MAP[api_name]["for-api"]
 
     headers = {
-        'x-rapidapi-key': os.getenv('API_KEY'),
-        'x-rapidapi-host': api['x-rapidapi-host'],
-        'Content-Type': 'application/json'
+        "x-rapidapi-key": os.getenv("RAPIDAPI_API_KEY"),
+        "x-rapidapi-host": api["x-rapidapi-host"],
+        "Content-Type": "application/json",
     }
 
-    return requests.get(
-        api['url'],
-        headers=headers,
-        params=api['payload']
-    )
+    return requests.get(api["url"], headers=headers, params=api["payload"])
+
 
 def main():
     # Comment out either one
-    apis_to_test = API_MAP.keys()       # Test all (Not recommended)
-    apis_to_test = API_MAP['api-name']  # Test desired (Change name)
+    apis_to_test = API_MAP.keys()  # Test all (Not recommended)
+    apis_to_test = API_MAP["api-name"]  # Test desired (Change name)
 
     for api in apis_to_test:
         print(f'Testing API: "{api}"')
         response = test_api(api)
-        print(response.json(), end='\n\n')
+        print(response.json(), end="\n\n")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
