ad9abffbcbde8b7b67c8f66fa57378e4541fd555
wz <wenzhe1423@gmail.com>
2026-04-21 11:49:56 +0800

    feat: fastAPI init and parser

---
+++ Backend/server/server.py
@@ -0,0 +1,55 @@
+import parser
+from fastapi import FastAPI
+from fastapi.datastructures import UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Allow CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def root():
+    print("Root is called")
+    return {"Message": "This is The Root of the Page"}
+
+
+# Take In Company Description, Inventory Sheets, Sales Sheets and Balance Sheets
+@app.post("/initialise_data")
+async def initialise_data(
+    description_file: UploadFile,
+    inventory_sheet: UploadFile,
+    sales_sheet: UploadFile,
+    balance_sheet: UploadFile,
+):
+
+    # Parse Files into Word and Dataframes
+    (
+        company_description,
+        inventory_df,
+        sales_df,
+        balance_sheet_df,
+    ) = await parser.initialise_data(
+        description_file, sales_sheet, inventory_sheet, balance_sheet
+    )
+
+    # Upload Data into ChromaDB
+    await upload_to_chromadb(company_description, "Company Description")
+
+    # Additional Processing for DataFrames
+    await parser.upload_dataframe_to_chromadb(inventory_df)
+    await parser.upload_dataframe_to_chromadb(sales_df)
+    await parser.upload_dataframe_to_chromadb(balance_sheet_df)
+
+    return {"status": "ok"}
+
+
+def upload_to_chromadb(content: str, metadata: str):
+    print(f"Uploading file {content} with metadata {metadata}")
