84aa9828cb2e636f9be48743fc933668cf153524
Colin <colinleonghongseng@gmail.com>
2026-04-21 14:56:02 +0800

    renamed long name to main.py

--- Backend/data_apis/__init__.py
+++ Backend/data_apis/__init__.py
@@ -1,3 +1,3 @@
-from .synthesise import fetch_and_summarise_trends
+from .main import main as fetch_and_summarise_trends
 
-__all__ = ["fetch_and_summarise_trends"]
+__all__ = ['fetch_and_summarise_trends']
